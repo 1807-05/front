fa73070720455367103336baf99209036bc2f72a
yangxi <438992320@qq.com>
2019-01-23 14:47:40 +0800

    添加了前台页面样式

---
+++ src/main/java/com/jk/controller/menu.java
@@ -0,0 +1,22 @@
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.View;
+
+/**
+ * @Description: 骚骚的🐏
+ * @Author: 杨
+ * @Date: 2019/1/23 14:42
+ */
+@Controller
+@RequestMapping("view")
+public class menu {
+    @RequestMapping("toview")
+    public String toView(String view){
+
+        return view;
+    }
+
+}
